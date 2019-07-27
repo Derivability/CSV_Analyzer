27e04a8396a2e6c8622c2d74e37a7b47d5a9e210
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-07-27 12:13:54 +0300

    Create Makefile
    Created Makefile, csv file and added program skeleton.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ELEMENT_SEPARATOR 	";"
+#define LINE_SEPARATOR 		";\n"
+#define SIZE			1024
+#define ROW			4
+#define COL			4
+
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+}
