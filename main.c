0b8f3de2acb57da2522c23323756846f4f5805b8
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-07-27 12:18:31 +0300

    Implement csv parsing
    Implemented csv table parsing to 2d array.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- main.c
+++ main.c
@@ -8,8 +8,36 @@
 #define ROW			4
 #define COL			4
 
+int** getMatrix(FILE *filename)
+{
+	int** matrix;
+	char line[SIZE];
+	char* tok;
+	matrix = (int **)malloc(ROW * sizeof(int *));
+	
+	for(int i = 0; i < ROW; i++) {
+		
+		fgets(line, SIZE, filename);
+		matrix[i] = (int *)malloc(COL * sizeof(int));
+		tok = strtok(line, ELEMENT_SEPARATOR);
+		
+		for(int j = 0; j < COL; j++) {
+			matrix[i][j] = atoi(tok);
+			tok = strtok(NULL, LINE_SEPARATOR);
+		}
+	}
+	return matrix;
+}
+
 int main(int argc, char* argv[])
 {
-	
+    if(argc > 1) {
+		FILE* csv_file = fopen(argv[1], "r");
+		int** data = getMatrix(csv_file);
+        fclose(csv_file);
+    }
+    else {
+		printf("No file specified\nExiting\n");
+	}
 	return 0;
 }
